634c2865e158eb99e41458676d0de3d74f881850
Yutman <kuriapeter95@gmail.com>
2024-04-09 15:27:29 +0300

    setting up customer review slider

---
+++ 06-slider/src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Embrace the glorious mess that you are.',
+  },
+  {
+    id: 2,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Simplicity is the ultimate sophistication.',
+  },
+  {
+    id: 3,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Design is not just what it looks like. Design is how it works.',
+  },
+  {
+    id: 4,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Leadership is doing what is right when no one is watching.',
+  },
+];
+
+export default people;
